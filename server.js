d4b4638b6feb961d4af034591186fab3dae49258
migmm <micmigm@gmail.com>
2022-11-22 22:54:05 -0300

    Index.html working

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 import express from 'express';
-//import routerProducts from './router/products.js';
-//import routerCart from './router/cart.js';
 
 import config from './config.js';
 
 const app = express();
 
-/* app.use(express.static('public', { extensions: ['html', 'htm'] }));
+app.use(express.static('public', { extensions: ['html', 'htm'] }));
 app.use(express.urlencoded({extended: true}));
-app.use(express.json()); */
+app.use(express.json());
 
-/* app.use('/api/products', routerProducts);
-app.use('/api/cart', routerCart); */
-
-
-/* app.get("/*", function (req, res) {
-    res.redirect("/#/404");
-}); */
+//app.use('/api/boats', routerBoats);
 
 const PORT = config.PORT;
 const server = app.listen(PORT, () => console.log(`Servidor Express escuchando en el puerto ${PORT}.`));
