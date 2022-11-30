178d186cded73c56df35a0c80bfecb923a50f3d6
migmm <micmigm@gmail.com>
2022-11-29 22:04:03 -0300

    Temporary clean

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from 'express';
-
 import config from './config.js';
 
 const app = express();
@@ -8,7 +7,7 @@ app.use(express.static('public', { extensions: ['html', 'htm'] }));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-//app.use('/api/boats', routerBoats);
+//app.use('/api/search', routerSearch);
 
 const PORT = config.PORT;
 const server = app.listen(PORT, () => console.log(`Servidor Express escuchando en el puerto ${PORT}.`));
